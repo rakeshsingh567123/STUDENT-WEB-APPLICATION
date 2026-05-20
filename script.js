f6ba0965dae7df380b2cc4f92d2aaf07aa311ab2
Rakesh Singh <rakeshsingh567123@gmail.com>
2026-05-20 11:52:21 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,8 @@
+function showData(){
+
+    let name = document.getElementById("name").value;
+    let age = document.getElementById("age").value;
+
+    document.getElementById("result").innerHTML =
+    "Name: " + name + "<br>Age: " + age;
+}
